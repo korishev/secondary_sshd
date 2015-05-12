15de34058f803f2321a3d95bae0f8c4517c7fa4c
Morgan Nelson <mnelson@steele-forge.com>
2015-05-12 13:51:55 -0500

    add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,6 @@ license          "Apache 2.0"
 description      "Installs/Configures secondary_sshd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+name             "secondary_sshd"
 
 depends          "openssh"
